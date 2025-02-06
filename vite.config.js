24ed3c695645d77c6cf1984749b70326ef48eec1
Daniel Garrido <86997452+Daniel-Garrido@users.noreply.github.com>
2025-02-05 23:41:34 -0600

    cambios al vite config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    
   ],
+  base:'https://Daniel-Garrido.github.io/Portafolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
